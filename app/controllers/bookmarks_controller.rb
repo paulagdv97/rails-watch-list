5b2488c627dc05c1ab062145d86486c54f27c0e4
paula garcia <paula.garciadv@gmail.com>
2021-11-12 15:34:06 +0100

    Create bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,17 @@
 class BookmarksController < ApplicationController
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   # POST /restaurants
   def create
+    raise
     @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to @list
+      redirect_to list_path(@list)
     else
       render :new
     end
@@ -16,6 +20,6 @@ class BookmarksController < ApplicationController
   private
   # Only allow a list of trusted parameters through.
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
